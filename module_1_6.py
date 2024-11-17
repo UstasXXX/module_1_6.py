b889c20cd269c8630c339dd4f7b6f609b726cfe3
UstasXXX <sirius20011990@gmail.com>
2024-11-17 16:10:23 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,19 @@
+# 1 задание
+
+my_dict = {'Yura' : 1990, 'Zlata' : 2018}
+print(my_dict)
+print(my_dict['Zlata'])
+print(my_dict.get('Julia'))
+my_dict.update({'father' : 1966 , 'mother' : 1972})
+my_dict_1 = my_dict.pop ('mother')
+print(my_dict_1)
+print(my_dict)
+
+# 2 задание
+my_set = {1 ,2, 'Zlata', 1, 2}
+print(my_set)
+my_set.add(4)
+my_set.add(5)
+my_set.discard(1)
+print(my_set)
+
